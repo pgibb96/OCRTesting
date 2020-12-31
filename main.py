8cf1fb41a57fc03f90f919c0401fd39569a94f5e
pgibb96 <34801002+pgibb96@users.noreply.github.com>
2020-12-31 15:51:08 -0500

    Setting up basic import of sudoku images

--- main.py
+++ main.py
@@ -1,1 +1,21 @@
+import matplotlib.pyplot as plt
 
+import os
+import skimage
+
+from skimage import io, data
+from skimage.color import rgb2gray, rgba2rgb
+from skimage.filters import threshold_mean
+
+sudokus = []
+directory = "./images/"
+for filename in os.listdir(directory):
+    sudokus.append(os.path.join(directory, filename))
+
+for sudoku in sudokus:
+    temp = io.imread(sudoku)
+    grayscale = rgb2gray(rgba2rgb(temp))
+    thresh = threshold_mean(grayscale)
+    binary = grayscale > thresh
+    io.imshow(binary)
+    io.show()
